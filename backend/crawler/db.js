8c3569b71b0a1c7e66b4aa44dbf1d2accb41753f
radheyyyyyy <mahiradhey0204@gmail.com>
2025-03-24 21:34:46 +0530

    mongo added

--- backend/crawler/db.js
+++ backend/crawler/db.js
@@ -1,14 +1,32 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/").then(()=> {
+mongoose.connect("mongodb+srv://mahiradhey0204:entra@cluster0.kc5ch1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=> {
     console.log("Connected to mongoDB")
 });
 const schema=mongoose.Schema({
     link:String,
     source:String,
-    data:String
+    title:String,
+    date:{
+        type:Date,default:Date.now
+    },
+    isFresh:{
+        type:Boolean,default:true
+    }
 })
 const news=mongoose.model('news',schema);
+
+const changeStream=news.watch();
+
+changeStream.on("change",(change)=>{
+    if(change.operationType==='insert'){
+        console.log("inserted to postgres")
+    }
+    else if(change.operationType==="update"){
+        console.log(change)
+    }
+
+})
 
 module.exports={
     news
